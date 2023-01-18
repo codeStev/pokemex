20e6a1315ac53939ca871f0a42b0bbdedf2f3426
codestev <codestev@outlook.de>
2023-01-18 02:09:44 +0100

    initial commit

---
+++ api/src/main/java/de/codestev/PokemexApiApplication.java
@@ -0,0 +1,11 @@
+package de.codestev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemexApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PokemexApiApplication.class, args);
+    }
+}
